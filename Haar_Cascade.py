3f4ea566d5ad2a2e899d9b354d83a9f61f663f95
Devershi Chandra <devershichandra27@gmail.com>
2017-03-16 23:00:50 +0530

    Updated Haar_Cascade.py
    Changed the program to recognise and respond to open palm gesture and open home directory, and display the message "Smile Detected" when user smiles :-)

--- Haar_Cascade.py
+++ Haar_Cascade.py
@@ -1,24 +1,28 @@
+import os
 import cv2
 
-face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye = cv2.CascadeClassifier('haarcascade_eye.xml')
+fingertip = cv2.CascadeClassifier('fingertips.xml')
+smile = cv2.CascadeClassifier('smile.xml')
 
 cap = cv2.VideoCapture(0)
 
 while True:
 	ret, inputImage = cap.read()
 	grey = cv2.cvtColor(inputImage, cv2.COLOR_BGR2GRAY)
-	faces = face.detectMultiScale(grey, 1.2, 5)
 
-	for (x, y, w,h) in faces:
-		cv2.rectangle(inputImage, (x,y), (x+w, y+h), (255, 0, 0), 2)
-		cv2.putText(inputImage,"Face Detected!!!", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1 	, 255)
-		roiGrey = grey[y:y+h, x:x+w]
-		roiColor = inputImage[y:y+h, x:x+w]
-		eyes = eye.detectMultiScale(roiGrey, 1.3, 5)
-		for (ex, ey, ew, eh) in eyes:
-			cv2.rectangle(roiColor, (ex, ey), (ex+ew, ey+eh), (0, 255,0), 2)
-			cv2.putText(inputImage,"Eyes Detected!!!", (ex,ey), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+	fingertips = fingertip.detectMultiScale(grey, 1.4, 4)
+	smileys = smile.detectMultiScale(grey, 1.2, 4 )
+	i=0
+	for (x,y,h,w) in fingertips:
+		i+=1
+		if i==8:
+			cap.release()
+			cv2.putText(inputImage, "Open Home", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+			os.system('nautilus ')
+			break
+	for (x,y,h,w) in smileys:
+		cv2.putText(inputImage, "Smile Detected", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+		pass
 
 	cv2.imshow('img', inputImage)
 	k=cv2.waitKey(30) & 0xff
